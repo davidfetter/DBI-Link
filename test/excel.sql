56f429091768d72aad81f8fd2e1b6c696ec5719f
David Fetter <david@fetter.org>
2005-01-05 08:01:45 +0000

    Added excel test. Head hurts from monitor bruises. :P

---
+++ test/excel.sql
@@ -0,0 +1,25 @@
+/* 
+ * Data source:     dbi:Excel:file=settings.xls
+ * User:            NULL
+ * Password:        NULL
+ * dbh attributes:  NULL
+ * remote schema:   NULL
+ * remote catalog:  NULL
+ * local schema:    excel
+ */
+
+SELECT set_config(
+  'search_path'
+, 'dbi_link,' || current_setting('search_path')
+, false
+);
+
+SELECT make_accessor_functions(
+  'dbi:dbi:Excel:file=settings.xls'
+, NULL
+, NULL
+, NULL
+, NULL
+, NULL
+, 'excel'
+);
