ae35eda72ec8a387eb454ba5a81c8d7329f1f98b
David Fetter <david@fetter.org>
2013-11-09 15:35:56 -0800

    *sigh*  My bad habits of yesteryear still haunt me.

--- examples/excel/excel.sql
+++ examples/excel/excel.sql
@@ -11,12 +11,12 @@
 SELECT dbi_link.ensure_in_search_path('dbi_link');
 
 SELECT make_accessor_functions(
-  'dbi:Excel:file=settings.xls'
-, NULL
-, NULL
-, NULL
-, NULL
-, NULL
-, NULL
-, 'excel'
+    'dbi:Excel:file=settings.xls',
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    'excel'
 );
